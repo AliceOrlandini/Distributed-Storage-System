e4b4d81a9399d9951700e8c334ef27915762e7a1
AliceOrlandini <a.orlandini10@studenti.unipi.it>
2025-03-15 11:30:13 +0100

    slave setup

--- server/slave/src/slave_app.erl
+++ server/slave/src/slave_app.erl
@@ -10,9 +10,77 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Port = case os:getenv("PORT") of
+        false ->
+            5001;
+        PortStr ->
+            io:format("PortStr: ~p~n", [PortStr]), 
+           {PortInt, _} = string:to_integer(PortStr),
+            PortInt
+    end,
+    io:format("Starting slave app on port ~p~n", [Port]),  
+    NumReplicas = case application:get_env(ws, num_replicas) of
+        {ok, N} -> N;
+        undefined -> 0
+    end,
+    io:format("Number of replicas: ~p~n", [NumReplicas]),
+
+    IsBootstrap = case application:get_env(ws, bootstrap) of
+        {ok, B} -> B;
+        undefined -> false
+    end,
+    Nodes = start_nodes(IsBootstrap, NumReplicas),
+    io:format("Number of replicas: ~p~n", [Nodes]),
+
+    lists:foreach(fun(Node) ->
+        io:format("Ping ~p: ~p~n", [Node, net_adm:ping(Node)])
+    end, Nodes),
+
+    % spawn a new process to handle the connections
+    spawn(?MODULE, loop/0, []).
+
     slave_sup:start_link().
 
 stop(_State) ->
     ok.
 
 %% internal functions
+start_nodes(false, _NumReplicas) ->
+    [];
+
+start_nodes(true, NumReplicas) ->
+    add_node(NumReplicas,[]).
+
+
+add_node(NumReplicas, Nodes) when NumReplicas > 0 -> 
+    Port = 5000 + NumReplicas,
+    NameStr = "slave" ++ integer_to_list(NumReplicas),
+    Name = list_to_atom(NameStr),
+    Args = "-setcookie slave -env PORT " ++ integer_to_list(Port),
+    {ok, Node} = slave:start("127.0.0.1", Name, Args),
+    rpc:call(Node, application, set_env, [ws, bootstrap, false]),
+    lists:foreach(
+        fun(Path) ->
+            rpc:call(Node, code, add_pathz, [Path])
+        end,
+        code:get_path()
+    ),
+    io:format("Started node ~p on port ~p~n", [Node, code:get_path()]),
+    Res = rpc:call(Node, application, ensure_all_started, [slave]),
+    io:format("Response: ~p~n", [Res]),
+    add_node(NumReplicas - 1, Nodes++[Node]);
+
+add_node(0, Nodes) ->
+    Nodes.
+
+loop() ->
+    receive
+        % receive a message from the master with the file name and content
+        {file, FileName, FileContent} ->
+            io:format("Received file: ~p~nContaining: ~p~n", [FileName, FileContent]),
+            loop();
+
+        Other ->
+            io:format("Messaggio unknown: ~p~n", [Other]),
+            loop()
+    end.
