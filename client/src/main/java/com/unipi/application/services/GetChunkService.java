300777da350f468e34a9224e2e043c86a58413a1
Kevin Boni <109586672+KevinBoni98@users.noreply.github.com>
2025-03-20 17:20:21 +0100

    fix client get data

--- client/src/main/java/com/unipi/application/services/GetChunkService.java
+++ client/src/main/java/com/unipi/application/services/GetChunkService.java
@@ -3,6 +3,7 @@ package com.unipi.application.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -21,29 +22,26 @@ public class GetChunkService {
             String backendUrl = filePositionModel.getIp();
             LOGGER.info("Fetching chunk for file position {}", filePositionModel.getIp());
 
-            ChunckModel chunk = WebClient.create(backendUrl)
+            byte[] data = WebClient.create(backendUrl)
                     .get()
                     .uri(uriBuilder -> uriBuilder
                             .path("/download")
                             .queryParam("token", filePositionModel.getChunkName())
                             .build())
                     .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                    .accept(MediaType.APPLICATION_OCTET_STREAM)
                     .exchangeToMono(response -> {
                         if (response.statusCode().isError()) {
                             LOGGER.error("Error fetching chunk, status code: {}", response.statusCode());
                             return Mono.empty();
                         } else {
-                            return response.bodyToMono(ChunckModel.class);
+                            return response.bodyToMono(byte[].class);
                         }
                     })
                     .block();
 
-            if (chunk == null || chunk.getData() == null || chunk.getData().length == 0) {
-                LOGGER.error("Error fetching chunk: received null or empty data");
-                return null;
-            }
-
-            // set the position of the chunk
+            ChunckModel chunk = new ChunckModel();
+            chunk.setData(data);
             chunk.setPositionFromFilePosition(filePositionModel);
             return chunk;
         } catch (Exception e) {
